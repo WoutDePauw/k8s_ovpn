2260e5a4c5d2c89486371703176ca092cfec0af4
WoutDePauw <wout.depauw@hotmail.com>
2018-05-08 11:05:58 +0200

    Update create_server_cert.sh

--- create_server_cert.sh
+++ create_server_cert.sh
@@ -1,5 +1,7 @@
 openssl genrsa -out server.key 2048
 chmod 600 server.key
-cat /server/ca.srl
-openssl req -new -key server.key -out server.csr -subj /CN=vpn.team05.labci.eu/
-openssl x509 -req -in server.csr -out server.crt -CA /server/ca.crt -CAkey /server/ca.key -CAserial /server/ca.srl -days 365
+{
+  openssl req -new -key server.key -out server.csr -subj /CN=vpn.team05.labci.eu/
+} || echo 'could not create request'
+  openssl x509 -req -in server.csr -out server.crt -CA /server/ca.crt -CAkey /server/ca.key -CAserial /server/ca.srl -days 365
+} || echo 'could not sign cert'
