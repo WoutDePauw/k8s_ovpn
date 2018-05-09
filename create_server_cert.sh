c4aa2075642ed11395beef69cba53a002e73ea43
root <root@localhost.localdomain>
2018-05-09 07:57:05 +0200

    added code to create tunnel interface

--- create_server_cert.sh
+++ create_server_cert.sh
@@ -6,3 +6,8 @@ chmod 600 /server.key
 {
   openssl x509 -req -in /server.csr -out /server.cert -CA /server/cert/ca.crt -CAkey /server/key/ca.key -CAserial /server/srl/ca.srl -days 365
 } || echo 'could not sign cert'
+
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
